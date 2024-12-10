7992019d9981f1d9917d4dc8ca3714b4ca8fa6d9
evaperisteri <evangelia.peristeri.g@gmail.com>
2024-12-10 20:10:20 +0200

    initial commit

---
+++ src/gr/aueb/cf/ch1/HelloWorld.java
@@ -0,0 +1,14 @@
+package gr.aueb.cf.ch1;
+
+/**
+ * Prints the phrase 'Hello World'
+ * in the standard output (console).
+ *
+ * @author xyz
+ */
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
