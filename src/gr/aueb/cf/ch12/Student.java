cad34ba14a6cc4985813d42e0567007b3dbb990e
evaperisteri <evangelia.peristeri.g@gmail.com>
2025-01-07 17:25:25 +0200

    java beans

--- src/gr/aueb/cf/ch12/Student.java
+++ src/gr/aueb/cf/ch12/Student.java
@@ -1,7 +1,60 @@
 package gr.aueb.cf.ch12;
 
+/**
+ * POJO
+ * Java Bean (private πεδία, default constructor, getters και setters -ή mutators kai accessors-με σύμβαση)
+ */
 public class Student {
-    int id;
-    String firstname;
-    String lastname;
+    private static int studentsCount = 0; // μπορει να αρχικοποιηθεί και σε static block
+
+    private int id;
+    private String firstname;
+    private String lastname;
+
+
+//    default constructor αρχικοποιει σε ντιφολτ τιμεσ:
+//    public Student(){
+//        id=0;
+//        firstname=null;
+//        lastname=null;
+//    }
+    public Student() {
+        studentsCount++;
+    }
+
+//    overloaded constructor
+    public Student(int id, String firstname, String lastname){
+        this.id=id;
+        this.firstname=firstname;
+        this.lastname=lastname;
+        studentsCount++;
+    }
+
+    public static int getStudentsCount(){
+        return studentsCount;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
 }
--- src/gr/aueb/cf/ch12/StudentDemo.java
+++ src/gr/aueb/cf/ch12/StudentDemo.java
@@ -3,6 +3,26 @@ package gr.aueb.cf.ch12;
 public class StudentDemo {
 
     public static void main(String[] args) {
-        Student alice = new Student();
+        Student student = new Student(); //invoke default constructor
+        Student bob = new Student(2, "Bob", "D." );// inject τιμές στο instance απο τον overloaded constructor
+
+        //set & get σε περιπτωσεις public στιγμιότυπων με την τελεία
+        student.setId(1);               //        student.id = 1;
+        student.setFirstname("Alice");  //        student.firstname= "Alice";
+        student.setLastname("W.");      //        student.lastname="W.";
+
+        //        System.out.println("Id: " + student.id);
+        //        System.out.println("Firstname: " + student.firstname);
+        //        System.out.println("Lastname: " + student.lastname);
+        System.out.println("Id: " + student.getId());
+        System.out.println("Firstname: " + student.getFirstname());
+        System.out.println("Lastname: " + student.getLastname());
+
+        System.out.println("Id: " + bob.getId());
+        System.out.println("Firstname: " + bob.getFirstname());
+        System.out.println("Lastname: " + bob.getLastname());
+
+
+        System.out.println("Student's instances count: " + Student.getStudentsCount());
     }
 }
