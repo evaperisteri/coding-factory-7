3a8508d6c6bec83d4354a54301f7ba1924d02c8c
evaperisteri <evangelia.peristeri.g@gmail.com>
2025-02-11 21:57:50 +0200

    lambdas & reflections

--- src/gr/aueb/cf/ch20/lambdas/Main.java
+++ src/gr/aueb/cf/ch20/lambdas/Main.java
@@ -3,6 +3,7 @@ package gr.aueb.cf.ch20.lambdas;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.function.Consumer;
+import java.util.function.Predicate;
 
 public class Main {
     private static final List<Teacher> teachers = List.of
@@ -12,18 +13,35 @@ public class Main {
     public static void main(String[] args) {
         List<Teacher> teachers1 = getFilteredTeachers(teachers, teacher -> teacher.getId() == 2);
         teachers1.forEach(System.out::println);
+        printTeachers(teachers,teacher -> teacher.printInstanceTeacher());
+        printTeachers(teachers,Teacher::printInstanceTeacher);
+        printTeachers(teachers,teacher -> Teacher.printStaticTeacher(teacher));
+        printTeachers(teachers, Teacher::printStaticTeacher);
+        printTeachers(teachers, teacher -> System.out.println(teacher));
+        printTeachers(teachers, System.out::println);
     }
 
-    public static List<Teacher> getFilteredTeachers(List<Teacher> teachers, ITeacherFilter filter) {
-        List<Teacher> teachersToReturn = new ArrayList<>();
+public static List<Teacher> getFilteredTeachers(List<Teacher> teachers, ITeacherFilter filter) {
+    List<Teacher> teachersToReturn = new ArrayList<>();
 
-        for(Teacher teacher : teachers) {
-            if(filter.filterId(teacher)){
-                teachersToReturn.add(teacher);
-            }
+    for(Teacher teacher : teachers) {
+        if(filter.filterId(teacher)){
+            teachersToReturn.add(teacher);
         }
-        return teachersToReturn;
     }
+     return teachersToReturn;
+        }
+
+//    public static List<Teacher> getFilteredTeachers(List<Teacher> teachers, Predicate<Teacher> filter) {
+//        List<Teacher> teachersToReturn = new ArrayList<>();
+//
+//        for(Teacher teacher : teachers) {
+//            if(filter.test(teacher)){
+//                teachersToReturn.add(teacher);
+//            }
+//        }
+//        return teachersToReturn;
+//    }
 
     public static void printTeachers(List<Teacher> teachers, Consumer<Teacher> consumer){
         for(Teacher teacher : teachers){
